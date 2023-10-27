40d62a03e8c8b1f2870de35f7910b3c06b6da4e5
Gabriel Noberto (iamdevnoel) <contato.gabrielnoberto@gmail.com>
2023-10-26 23:13:14 -0300

    updated gerenerics, sets and maps studies

--- generics_sets_maps/application/Program2.java
+++ generics_sets_maps/application/Program2.java
@@ -1,4 +1,50 @@
 package generics_sets_maps.application;
 
+import generics_sets_maps.entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Program2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Set<LogEntry> logEntrySet= new HashSet<>();
+
+        //input
+        System.out.print("Enter file full path: ");
+        File file = new File(sc.nextLine());
+
+        //ler cada linha do arquivo
+        try (BufferedReader buffR = new BufferedReader(new FileReader(file))) {
+            String line = buffR.readLine();
+
+            while (line != null) {
+                //splitar as linhas do arquivo para separar os dados username e instant
+                String[] info = line.split(" ");
+                //usar essas informações separadas para construir um objeto logEntry
+                LogEntry logEntry = new LogEntry(info[0], Instant.parse(info[1]));
+                //adicionar o novo objeto ao Set
+                logEntrySet.add(logEntry);
+
+                line = buffR.readLine();
+            }
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+
+        //output
+        //Uma vez que o Set não repete elementos, a quantidade de objetos = tamanho do Set
+        System.out.println("Total users: " + logEntrySet.size());
+
+        sc.close();
+
+    }
+
 }
--- generics_sets_maps/entities/LogEntry.java
+++ generics_sets_maps/entities/LogEntry.java
@@ -1,4 +1,37 @@
 package generics_sets_maps.entities;
 
+import java.time.Instant;
+import java.util.Objects;
+
 public class LogEntry {
+
+    private final String username;
+    private final Instant instant;
+
+    public LogEntry(String username, Instant instant) {
+        this.username = username;
+        this.instant = instant;
+    }
+
+    /*
+    Override gerado automaticamente pela IDE, considerando apenas o atributo username
+    como parâmetro de comparação. Necessário para compor o compareTo() de um Set.
+    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LogEntry logEntry = (LogEntry) o;
+        return Objects.equals(username, logEntry.username);
+    }
+
+    /*
+    Override gerado automaticamente pela IDE, considerando apenas o atributo username
+    como parâmetro de comparação. Necessário para compor o compareTo() de um Set.
+    */
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
 }
